976bc4ba0acdd46e8b3017446ad0d19cabb91331
Matthew D Wilson <mattrick2214@gmail.com>
2025-02-01 21:59:50 -0600

    added json and found another problem

--- main.js
+++ main.js
@@ -1,7 +1,62 @@
+const galleryImages = [
+    {
+        src: "./assets/gallery/image1.jpg",
+        alt: "Thumbnail Image 1"
+    },
+    {
+        src: "./assets/gallery/image2.jpg",
+        alt: "Thumbnail Image 2"
+    },
+    {
+        src: "./assets/gallery/image3.jpg",
+        alt: "Thumbnail Image 3"
+    },
+    {
+        src: "./assets/gallery/img1.png",
+        alt: "Thumbnail Image 3"
+    },
+];
 
+const products = [
+    {
+      title: "AstroFiction",
+      author: "John Doe",
+      price: 49.9,
+      image: "./assets/products/img6.png"
+    },
+    {
+      title: "Space Odissey",
+      author: "Marie Anne",
+      price: 35,
+      image: "./assets/products/img1.png"
+    },
+    {
+      title: "Doomed City",
+      author: "Jason Cobert",
+      price: 0,
+      image: "./assets/products/img2.png"
+    },
+    {
+      title: "Black Dog",
+      author: "John Doe",
+      price: 85.35,
+      image: "./assets/products/img3.png"
+    },
+    {
+      title: "My Little Robot",
+      author: "Pedro Paulo",
+      price: 0,
+      image: "./assets/products/img5.png"
+    },
+    {
+      title: "Garden Girl",
+      author: "Ankit Patel",
+      price: 45,
+      image: "./assets/products/img4.png"
+    }
+  ];
 
 // Menu section
-
 function menuHandler() {
     document.querySelector("#open-nav-menu").addEventListener("click", function(){
         document.querySelector("header nav .wrapper").classList.add("nav-open");
@@ -12,12 +67,13 @@ function menuHandler() {
     });
 }
 
+
 function celsiusToFahr(temperature){
     let fahr = (temperature * 9/5) + 32;
     return fahr;
 }
-// Greeting Section
 
+// Greeting Section
 function greetingHandler() {
     let currentHour = new Date().getHours();
     let greetingText;
@@ -54,70 +110,93 @@ function greetingHandler() {
 
 }
 
+// Local Time Handlers
+function clockHandler() {
+    setInterval(function(){
+        let localTime = new Date();
+        
+        document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2,"0");
+        document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2,"0");
+        document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2,"0");
+    },1000);
+}
 
-setInterval(function(){
-    let localTime = new Date();
-    
-    document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2,"0");
-    document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2,"0");
-    document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2,"0");
-},1000);
+function galleryHandler() {
 
-const galleryImages = [
-    {
-        src: "./assets/gallery/image1.jpg",
-        alt: "Thumbnail Image 1"
-    },
-    {
-        src: "./assets/gallery/image2.jpg",
-        alt: "Thumbnail Image 2"
-    },
-    {
-        src: "./assets/gallery/image3.jpg",
-        alt: "Thumbnail Image 3"
-    },
-    {
-        src: "./assets/gallery/img1.png",
-        alt: "Thumbnail Image 3"
-    },
-];
-
-let mainImage = document.querySelector("#gallery > img");
-let thumbnails = document.querySelector("#gallery .thumbnails");
+    let mainImage = document.querySelector("#gallery > img");
+    let thumbnails = document.querySelector("#gallery .thumbnails");
 
 
-mainImage.src = galleryImages[0].src;
-mainImage.alt = galleryImages[0].alt;
+    mainImage.src = galleryImages[0].src;
+    mainImage.alt = galleryImages[0].alt;
 
 
-galleryImages.forEach(function(image, index){
-    let thumb = document.createElement("img");
-    thumb.src = image.src;
-    thumb.alt = image.alt;
-    thumb.dataset.arrayIndex = index;
-    thumb.dataset.selected = true;
-    thumb.dataset.selected = index === 0 ? true : false;
+    galleryImages.forEach(function(image, index){
+        let thumb = document.createElement("img");
+        thumb.src = image.src;
+        thumb.alt = image.alt;
+        thumb.dataset.arrayIndex = index;
+        thumb.dataset.selected = true;
+        thumb.dataset.selected = index === 0 ? true : false;
 
-    thumb.addEventListener("click", function(e){
-        let selectedIndex = e.target.dataset.arrayIndex;
-        let selectedImage = galleryImages[selectedIndex];
-        mainImage.src = selectedImage.src;
-        mainImage.alt = selectedImage.alt;
+        thumb.addEventListener("click", function(e){
+            let selectedIndex = e.target.dataset.arrayIndex;
+            let selectedImage = galleryImages[selectedIndex];
+            mainImage.src = selectedImage.src;
+            mainImage.alt = selectedImage.alt;
 
-        thumbnails.querySelectorAll("img").forEach(function(img){
-            img.dataset.selected = false;
+            thumbnails.querySelectorAll("img").forEach(function(img){
+                img.dataset.selected = false;
+            });
+
+            e.target.dataset.selected = true;
+            
+
         });
 
-        e.target.dataset.selected = true;
-        
+
+        thumbnails.appendChild(thumb);
+    });
+}
+// Products Section
+
+// <div class="product-item">
+// <img src="./assets/products/img6.png" alt="AstroFiction">
+// <div class="product-details">
+//    <h3 class="product-title">AstroFiction</h3>
+//    <p class="product-author">John Doe</p>
+//    <p class="price-title">Price</p>
+//    <p class="product-price">$ 49.90</p>
+// </div>
+// </div>
+
+function productsHandler() {
+
+    let productsSection = document.querySelector(".products-area");
+    
+
+    products.forEach(function(product, index){
+        let productElm = document.createElement("div");
+        productElm.classList.add("product-item");
+
+        let productImage = document.createElement("img");
+        productImage.src = product.image;
+        productImage.alt = "Image for " + product.title;
+
+        productElm.append(productImage);
+
+        productsSection.append(productElm);
+         
 
     });
 
+};
 
-    thumbnails.appendChild(thumb);
-});
 
 // Page Load
 
 menuHandler();
 greetingHandler();
+clockHandler();
+galleryHandler();
+productsHandler();
