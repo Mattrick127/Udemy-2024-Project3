423f7fec28b79da7501362c3601e5dcc54a85689
Matthew D Wilson <mattrick2214@gmail.com>
2025-02-01 21:16:40 -0600

    fixed celsius to fahr button and wrote in commands.

--- main.js
+++ main.js
@@ -2,40 +2,57 @@
 
 // Menu section
 
-document.querySelector("#open-nav-menu").addEventListener("click", function(){
-    document.querySelector("header nav .wrapper").classList.add("nav-open");
-});
+function menuHandler() {
+    document.querySelector("#open-nav-menu").addEventListener("click", function(){
+        document.querySelector("header nav .wrapper").classList.add("nav-open");
+    });
 
-document.querySelector("#close-nav-menu").addEventListener("click", function(){
-    document.querySelector("header nav .wrapper").classList.remove("nav-open");
-});
-
-// Greeting Section
-
+    document.querySelector("#close-nav-menu").addEventListener("click", function(){
+        document.querySelector("header nav .wrapper").classList.remove("nav-open");
+    });
+}
 
 function celsiusToFahr(temperature){
     let fahr = (temperature * 9/5) + 32;
     return fahr;
 }
+// Greeting Section
 
-const greetingText = "Good morning!";
-const weatherCondition = "sunny";
-const userLocation = "San Antonio";
-let temperature = 25;
+function greetingHandler() {
+    let currentHour = new Date().getHours();
+    let greetingText;
 
-let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it\’s ${celsiusToFahr(temperature).toFixed(1)}°C outside.`;
-let fahrText = `The weather is ${weatherCondition} in ${userLocation} and it\’s ${celsiusToFahr(temperature).toFixed(1)}°F outside.`;
+    if (currentHour < 12) {
+        greetingText = "Good Morning!";
+    } else if (currentHour <19) {
+        greetingText = "Good Afternoon!";
+    } else if (currentHour < 24) {
+        greetingText = "Good Evening!";
+    } else {
+        greetingText = "Welcome!";
+    };
 
-document.querySelector("#greeting").innerHTML = greetingText;
-document.querySelector("#weather").innerHTML = celsiusText;
 
-document.querySelector(".weather-group").addEventListener("click", function(e){
-    if (e.target.id == "celsius") {
-        document.querySelector("p#weather").innerHTML = celsiusText;
-    }   else if (e.target.id == "fahr") {
-        document.querySelector("p#weather").innerHTML = fahrText;
-    }
-});
+
+    const weatherCondition = "sunny";
+    const userLocation = "San Antonio";
+    let temperature = 25;
+
+    let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it\’s ${temperature.toFixed(1)}°C outside.`;
+    let fahrText = `The weather is ${weatherCondition} in ${userLocation} and it\’s ${celsiusToFahr(temperature).toFixed(1)}°F outside.`;
+
+    document.querySelector("#greeting").innerHTML = greetingText;
+    document.querySelector("#weather").innerHTML = celsiusText;
+
+    document.querySelector(".weather-group").addEventListener("click", function(e){
+        if (e.target.id == "celsius") {
+            document.querySelector("p#weather").innerHTML = celsiusText;
+        }   else if (e.target.id == "fahr") {
+            document.querySelector("p#weather").innerHTML = fahrText;
+        }
+    });
+
+}
 
 
 setInterval(function(){
@@ -57,6 +74,10 @@ const galleryImages = [
     },
     {
         src: "./assets/gallery/image3.jpg",
+        alt: "Thumbnail Image 3"
+    },
+    {
+        src: "./assets/gallery/img1.png",
         alt: "Thumbnail Image 3"
     },
 ];
@@ -95,3 +116,8 @@ galleryImages.forEach(function(image, index){
 
     thumbnails.appendChild(thumb);
 });
+
+// Page Load
+
+menuHandler();
+greetingHandler();
