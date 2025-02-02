b28dfa87ade04e4bd569ece443cd56f6135b5927
Matthew D Wilson <mattrick2214@gmail.com>
2025-02-01 20:19:07 -0600

    fixed gallery thumbnails

--- main.js
+++ main.js
@@ -61,16 +61,28 @@ const galleryImages = [
     },
 ];
 
-
-// for (let i in galleryImages) {
-//     console.log(galleryImages[i]);
-// }
-
 let mainImage = document.querySelector("#gallery > img");
+let thumbnails = document.querySelector("#gallery .thumbnails");
+
+
 mainImage.src = galleryImages[0].src;
 mainImage.alt = galleryImages[0].alt;
 
 
 galleryImages.forEach(function(image, index){
+    let thumb = document.createElement("img");
+    thumb.src = image.src;
+    thumb.alt = image.alt;
+    thumb.dataset.arrayIndex = index;
+    thumb.dataset.selected = true;
+
+    if(index === 0) {
+        thumb.dataset.selected = true;
+    } else{
+        thumb.dataset.selected = false;
+    }
+
+
+    thumbnails.appendChild(thumb);
 
 });
